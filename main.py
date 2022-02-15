02a57ffd91fc3a1485c964a5d4503b74e4276251
Giga <gigabyte5671@gmail.com>
2022-02-16 12:20:50 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from discord.ext import commands, tasks
+from discord.ext import commands
 from urllib.request import urlopen
 import json
 import os
@@ -7,7 +7,7 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix='?') #define command decorator
 
-userCommands = ['help', 'hello', 'online']
+userCommands = ['help', 'hello', 'about', 'online']
 
 urls = {'steam': 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=17080',
 		'community': 'http://ta.kfk4ever.com:9080/status'}
@@ -15,51 +15,47 @@ urls = {'steam': 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurren
 responses = {'steam': '',
 			 'community': ''}
 
-responses['steam'] = json.loads(urlopen(urls['steam']).read())
-responses['community'] = json.loads(urlopen(urls['community']).read())
+def getOnlinePlayers():
+	responses['steam'] = json.loads(urlopen(urls['steam']).read())
+	responses['community'] = json.loads(urlopen(urls['community']).read())
 
 
 
 @bot.event
 async def on_message(message):
-    await bot.process_commands(message)
-
-
-#Show list of commands
-@bot.command(pass_context=True)
-async def help(ctx):
-    await ctx.channel.send('[VVH] The available commands are: ')
-    new_message = "" 
-    for com in userCommands:
-        new_message += com + ", "
-    new_message += "."
-    await ctx.channel.send(new_message)
+	await bot.process_commands(message)
 
 
 #Say Hello
 @bot.command(pass_context=True)
 async def hello(ctx):
-    await ctx.send("[VGH] Hello! I'm the Wilderzone Servers bot :wave:")
+	await ctx.send("[VGH] Hello! I'm the Wilderzone Servers bot :wave:")
+
+
+#About this bot
+@bot.command(pass_context=True)
+async def introduce(ctx):
+	await ctx.send("[VGH] Hello! I'm the Wilderzone Servers bot :wave:\nI can tell you how many people are playing Tribes Ascend at any time!\nJust say `?online` in any channel and I'll reply. :tada:")
 
 
 #List online players
 @bot.command(pass_context=True)
 async def online(ctx):
-    steam = responses['steam']['response']['player_count']
-    community = responses['community']['online_players']
-    total = steam + community
-    await ctx.send("There are currently " + total + " players online.")
-    message = "HiRez Servers: " + steam
-    await ctx.send(message)
-    message = "Community Servers: " + community
-    await ctx.send(message)
+	getOnlinePlayers()
+	steam = responses['steam']['response']['player_count']
+	community = responses['community']['online_players']
+	total = steam + community
+	message = "There are currently `" + str(total) + "` players online.\n"
+	message += " • HiRez Servers: `" + str(steam) + "`\n"
+	message += " • Community Servers: `" + str(community) + "`"
+	await ctx.send(message)
 
 
 
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+	print('We have logged in as {0.user}'.format(bot))
 
 
 bot.run(os.getenv('TOKEN'))
