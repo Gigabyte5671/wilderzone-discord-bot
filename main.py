5dba3e2883854ef6751068b062d851496a348aa7
Giga <gigabyte5671@gmail.com>
2022-02-16 16:03:26 +1300

    Add ?links command
    Sends a message containing a list of useful community links.

--- main.py
+++ main.py
@@ -7,7 +7,23 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix='?') #define command decorator
 
-userCommands = ['help', 'hello', 'about', 'online']
+userCommands = ['help', 'hello', 'about', 'links', 'online']
+
+community_links = [
+	{'title': 'Wilderzone Live', 'short_title': 'Wilderzone', 'url': 'https://wilderzone.live/'},
+	{'title': 'Wilderzone Servers', 'short_title': 'Wilderzone Servers', 'url': 'https://servers.wilderzone.live/'},
+	{'title': 'Llamagrab Servers', 'short_title': 'Llamagrab Servers', 'url': 'https://servers.llamagrab.net/'},
+	{'title': 'Tribes Launcher Sharp', 'short_title': 'Launcher', 'url': 'https://github.com/mcoot/TribesLauncherSharp/releases/latest'},
+	{'title': 'TAMods', 'short_title': 'TAMods', 'url': 'https://www.tamods.org/'},
+	{'title': 'TAAGC', 'short_title': 'TAAGC', 'url': 'https://taagc.org/'},
+	{'title': "Dodge's Domain", 'short_title': "Dodge's Domain", 'url': 'https://www.dodgesdomain.com/'},
+	{'title': 'Tribes Ascend on Steam', 'short_title': 'Steam', 'url': 'https://store.steampowered.com/app/17080/Tribes_Ascend/'},
+	{'title': 'Tribes on Reddit', 'short_title': 'Subreddit', 'url': 'https://www.reddit.com/r/Tribes/'},
+	{'title': 'Tribes Lore', 'short_title': 'Lore', 'url': 'https://tribes.fandom.com/wiki/Backstory_timeline'},
+	{'title': 'Images from Tribes Ascend', 'short_title': 'Gallery', 'url': 'https://wilderzone.live/gallery/'},
+	{'title': 'Music from Tribes Ascend', 'short_title': 'Music', 'url': 'https://www.youtube.com/playlist?list=PLBAC1B18024809938'},
+	{'title': 'Tribes Universe', 'short_title': 'Tribes Universe', 'url': 'https://www.tribesuniverse.com/'}
+]
 
 urls = {'steam': 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=17080',
 		'community': 'http://ta.kfk4ever.com:9080/status'}
@@ -36,6 +52,15 @@ async def hello(ctx):
 @bot.command(pass_context=True)
 async def introduce(ctx):
 	await ctx.send("[VGH] Hello! I'm the Wilderzone Servers bot :wave:\nI can tell you how many people are playing Tribes Ascend at any time!\nJust say `?online` in any channel and I'll reply. :tada:")
+
+
+#Links
+@bot.command(pass_context=True)
+async def links(ctx):
+	message = "**Here are some useful community links:** \n"
+	for link in community_links:
+		message += " • " + link['short_title'] + ": " + link['url'] + "\n"
+	await ctx.send(message)
 
 
 #List online players
